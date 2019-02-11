06ce237182865506ac0fe64c5b6622530904be19
emrtbrg <emrtbrg@eselnts1219.mo.sw.ericsson.se>
2019-02-11 11:57:27 +0100

    File name day1part2 finally works, last commit

--- day1_part2.tcl
+++ day1_part2.tcl
@@ -3,43 +3,33 @@
 #Solution to Advent of Code puzzle for Day 1 (Part 2)\
 exec tclsh "$0" "$@"
 
-#set input [open "day1.txt" r]
-#close $input
+set file [open "day1.txt" r]
+set _input [read $file]
+close $file
 
-proc read_file {_input_file_path} {
-    
-    set _input [open $_input_file_path r]
-    set _data  [read $_input]
 
-    close $_input
-    return $_data
+proc part1 {_input} {
+    return [::tcl::mathop::+ {*}$_input]
 }
 
-set _freq_diffs [regsub "\n" [read_file "day1.txt"] ""]
+proc part2 {freqs startFreq _input} {
+	dict set freqs $startFreq 1
+	set sum $startFreq
 
-set _mytext "your sum frequency is: "
-lappend _mytext [expr $_freq_diffs]
-puts $_mytext
-
-#part2 begins
-
-proc part2 {freqs startFreq input} {
-  dict set freqs $startFreq 1
-    set sum $startFreq	
-	foreach line $input {
-      	  if {$line != ""} {       
-	   set sum [expr {$sum + $line}]
-		if {[dict exists $freqs $sum]}{
-			return $sum 
-	} else { 
-    		dict set freqs $sum 1
-    
-		}
-	     }
-	}
- 
-tailcall part2 $freqs $sum $input 
-
+foreach line $_input {
+	
+	if {$line != ""} {
+	
+	set sum [expr {$sum + $line}] 
+ if {[dict exists $freqs $sum]} {
+        return $sum
+   } else {
+        dict set freqs $sum 1
+      }
+    }
+  }
+   part2 $freqs $sum $_input
 }
-puts "part1: [part1 $input]"
-puts "part2: [part2 {} 0 $input]"
+
+puts "The answer to part 1 is: [part1 $_input]"
+puts " The answer to part 2 is: [part2 {} 0 $_input]"
