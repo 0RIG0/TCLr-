2616cab9eeefab99a9605265cfaf8734e654a483
emrtbrg <emrtbrg@eselnts1219.mo.sw.ericsson.se>
2019-02-12 09:53:54 +0100

    Part 2 working on the solution

---
+++ day2_part1.tcl
@@ -0,0 +1,14 @@
+#!/bin/sh
+# the next line restarts using tclsh \
+exec tclsh "$0" "$@"
+
+if {false} {
+	To make sure you didn't miss any, you scan the 
+	likely candidate boxes again, counting the number that have an
+	ID containing exactly two of any letter and then separately counting 
+	those with exactly three of any letter. You can multiply those two 
+	counts together to get a rudimentary
+	checksum and compare it to what your device predicts.
+}
+
+
