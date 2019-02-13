f6b0290e1aab9e27fd9317c3f8598ed849aee423
emrtbrg <emrtbrg@eselnts1219.mo.sw.ericsson.se>
2019-02-13 08:57:42 +0100

    Finished

--- day2_part1.tcl
+++ day2_part1.tcl
@@ -15,13 +15,32 @@ set _file [open "day2.txt" r]
 set _input [read $_file]
 close $_file
 # Match the first substring with lowercase letters only
-#
-join $_input ","
+# 
+ proc part1 {_input} {
+ 	set numDuplicates [dict create] 
+ 	foreach id $_input { 
+ 		set numIncreased [dict create] 
+ 		set freqs [dict create] 
+ 		foreach c [split $id {}] { 
+ 		dict incr freqs $c 
+    } 
+    dict for {c num} $freqs { 
+      if {![dict exists $numIncreased $num]} {
+               dict incr numDuplicates $num        
+                 dict set numIncreased $num 1 
+       } 
+     } 
+   } 
+   dict with numDuplicates {return [expr {$2 * $3}]} 
+ } 
+puts "part1: [part1 $_input]"
+#join $_input 
 #set result [regexp {[A-Za-z]+} $_input match] ;#all letters (uppercase and lowercase)
 #puts "Result: $result match: $match" ;# prints out Result: 1 match: fzostwblnqkhpuzxirnevmaycq
 
-puts [expr {[llength [split $_input "a"]] - 1}] ;#prints out that there are 252 a's
-puts "there are :[expr {[llength [split $_input "a"]] - 1}] number of a'ss in the box ids"
+#puts [expr {[llength [split $_input "a"]] - 1}] ;#prints out that there are 252 a's
+#puts "there are :[expr {[llength [split $_input "a"]] - 1}] number of a's in the box ids"
+
 #puts "first occurance of $s2 in $_input"
 #puts [string first  $s2 $_input] ;#prints out at index 22 line 1
 #want to find ALL occurances of a in my text file
